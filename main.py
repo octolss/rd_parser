40976738825423a16478f48b04f4831bf84e48bd
octolss <147706639+octolss@users.noreply.github.com>
2023-12-18 21:28:28 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import requests
+from bs4 import BeautifulSoup as BS
 
+s = requests.Session()
+response = s.get(url="https://www.rdveikals.lv/categories/lv/150/sort/5/filter/0_0_0_0/page/1/Portat%C4%ABvie-datori.html")
+
+with open("index.html", "w") as file:
+    file.write(response.text)
+
+with open("index.html", "r") as file:   
+    soup = BS(file, 'html.parser')
+
+d = [i for i in soup.find_all('p') if i[] == "€"]
+c = 0
+
+for link in soup.find_all('h3'):
+
+    a = link.get_text('a')
+    with open("index.txt", "a") as file:
+        file.write(a[2:len(a)-3])
+        file.write(d[c].get_text())
+    c+=1
---
+++ makefile
@@ -0,0 +1,4 @@
+run:
+	git add .
+	git commit -a -m "."
+	git push
